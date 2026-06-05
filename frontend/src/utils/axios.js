6b9e1d14aadb7a2bcabb78b25ae1744987bea5fa
GOUTAM GUPTA <inspiritualstrainge02@gmail.com>
2026-06-05 16:04:56 +0530

    Refactor axios configuration to use API_HOST

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
+const API_HOST = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: `${API_HOST}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+// (keep your existing interceptor code below this line)
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+  if (token) config.headers["Authorization"] = `Bearer ${token}`;
+  return config;
 });
-
 
 export default api;
